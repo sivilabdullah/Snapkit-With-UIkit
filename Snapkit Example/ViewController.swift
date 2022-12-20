c2d77f8962dde403e06872fb50f0a9d7722befd9
sivilabdullah <194308002@ogr.ebyu.edu.tr>
2022-12-20 12:27:02 +0300

    Initial Commit

---
+++ Snapkit Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapkit Example
+//
+//  Created by abdullah's Monterey on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
